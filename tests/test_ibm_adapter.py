bccc35cec2d95f40a37b2bf18c5c5b1d70f3932b
NhonLV <44114705+nhonlvsoict@users.noreply.github.com>
2025-08-11 17:20:14 +0100

    Test IBM adapter templating

---
+++ tests/test_ibm_adapter.py
@@ -0,0 +1,19 @@
+from qsg.adapters.ibm_qasm3 import IBMQasm3Adapter
+
+
+def test_entrypoint_uses_default_context():
+    adapter = IBMQasm3Adapter()
+    code = adapter.entrypoint()
+    assert '/app/payload/program.qasm' in code
+    assert 'os.getenv("IBM_TOKEN")' in code
+    assert 'No IBM_TOKEN env var found' in code
+
+
+def test_entrypoint_accepts_custom_context():
+    adapter = IBMQasm3Adapter(payload_path='/tmp/foo.qasm', token_env_var='MY_TOKEN')
+    code = adapter.entrypoint()
+    assert '/tmp/foo.qasm' in code
+    assert 'os.getenv("MY_TOKEN")' in code
+    assert 'No MY_TOKEN env var found' in code
+    assert '/app/payload/program.qasm' not in code
+    assert 'IBM_TOKEN' not in code
