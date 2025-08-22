75c29e1be70dde2244a2e9f8ebb9c5c948fbe304
NhonLV <44114705+nhonlvsoict@users.noreply.github.com>
2025-08-22 16:26:08 +0100

    fix new grover

--- examples/grover_new_qiskit.py
+++ examples/grover_new_qiskit.py
@@ -40,6 +40,28 @@ def diffuser_with_ancilla(n):
     qc.x(range(n)); qc.h(range(n))
     return qc
 
+# def build_circuit(n=None, marked=None):
+#     if n is None:
+#         n = int(os.getenv("QGEN_N", "4"))
+     
+#         print(f"Using n={n} from environment variable QGEN_N")
+#     if marked is None:
+#         default = "0"*(n-1) + "1"
+#         marked = os.getenv("QGEN_MARKED", default)[:n]
+#         print(f"Using marked={marked} from environment variable QGEN_MARKED")
+#     iters = max(1, min(2, floor(pi/4 * sqrt(2**n))))  # hardware-friendly
+#     qc = QuantumCircuit(n+1, n)  # +1 ancilla
+#     qc.h(range(n))
+#     O = oracle_with_ancilla(n, marked)
+#     D = diffuser_with_ancilla(n)
+#     for _ in range(iters):
+#         qc.compose(O, inplace=True)
+#         qc.compose(D, inplace=True)
+#     # measure only the data qubits
+#     qc.measure(range(n), range(n))
+#     return qc
+
+    
 def build_circuit(n=None, marked=None):
     if n is None:
         n = int(os.getenv("QGEN_N", "4"))
@@ -49,7 +71,8 @@ def build_circuit(n=None, marked=None):
         default = "0"*(n-1) + "1"
         marked = os.getenv("QGEN_MARKED", default)[:n]
         print(f"Using marked={marked} from environment variable QGEN_MARKED")
-    iters = max(1, min(2, floor(pi/4 * sqrt(2**n))))  # hardware-friendly
+    iters = floor(pi/4 * sqrt(2**n))
+    # iters = max(1, min(2, floor(pi/4 * sqrt(2**n))))  # clamp to 1..2 for hardware
     qc = QuantumCircuit(n+1, n)  # +1 ancilla
     qc.h(range(n))
     O = oracle_with_ancilla(n, marked)
