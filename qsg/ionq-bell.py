b079f344b7371ee4759d5ad16f3a38387e1549fc
NhonLV <44114705+nhonlvsoict@users.noreply.github.com>
2025-10-14 00:31:52 +0100

    Create ionq-bell.py

---
+++ qsg/ionq-bell.py
@@ -0,0 +1,31 @@
+# import Aer here, before calling qiskit_ionq_provider
+from qiskit import Aer
+from qiskit_ionq import IonQProvider 
+from qiskit import QuantumCircuit
+from qiskit.providers.jobstatus import JobStatus
+
+# Call provider and set token value
+provider = IonQProvider()
+
+
+
+# Create a bell state circuit.
+qc = QuantumCircuit(2, 2)
+qc.h(0)
+qc.cx(0, 1)
+qc.measure([0, 1], [0, 1])
+
+# Get an IonQ simulator backend to run circuits on:
+backend = provider.get_backend("ionq_simulator")
+
+# Then run the circuit:
+job = backend.run(qc, shots=1000)
+
+# Save job_id
+job_id_bell = job.job_id()
+
+# Fetch the result:
+result = job.result()
+
+
+print(result.get_counts())
