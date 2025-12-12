bcf62143ce214575919b387eb7a8321c3a4816ea
NhonLV <44114705+nhonlvsoict@users.noreply.github.com>
2025-12-12 00:49:31 +0000

    fix

--- examples/vht/vht_ibm.py
+++ examples/vht/vht_ibm.py
@@ -186,8 +186,8 @@ if not token:
     # samples = res[0].data["samples"]
     result_payload["backend_mode"] = "aer_local"
     # For primitives v0, use quasi_dists; newer APIs may have .quasi_dists or ._pub_results
-    # qd = getattr(res, "quasi_dists", None)
-    result_payload["counts"] = res
+    qd = getattr(res, "quasi_dists", None)
+    if qd: result_payload["counts"] = qd[0]
 else:
     #ibm_quantum_platform
     from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2 as Sampler
